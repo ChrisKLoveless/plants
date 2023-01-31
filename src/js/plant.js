cae82965ed9c8e50b6aee8a77cc206b55e0a535f
ChrisKLoveless <chriskloveless@gmail.com>
2023-01-31 11:52:16 -0800

    seperate state to plant objects
    co-authored-by: robbryandev@gmail.com

--- src/js/plant.js
+++ src/js/plant.js
@@ -1,73 +1,57 @@
 export let plant = {
-    water: 0,
-    soil: 0,
-    light: 0
-  }
+  id: 0,
+  water: 0,
+  soil: 0,
+  light: 0
+};
 
-  // const hydrate = (plant) => {
-  //   return {
-  //     ...plant,
-  //     water: (plant.water || 0) + 1
-  //   }
-  // }
+// eslint-disable-next-line no-unused-vars
+export function createPlant() {
+  let plantId = 0;
+  return function newPlant(thisWater = 0, thisSoil = 0, thisLight = 0) {
+    const newId = plantId + 1;
+    plantId = newId;
+    return { water: thisWater, soil: thisSoil, light: thisLight, id: newId };
+  };
+}
 
-  // const feed = (plant) => {
-  //   return {
-  //     ...plant,
-  //     soil: (plant.soil || 0) + 1
-  //   }
-  // }
+export const giveLight = (plant) => {
+  return {
+    ...plant,
+    light: (plant.light || 0) + 1
+  };
+};
 
-  export const giveLight = (plant) => {
-    return {
-      ...plant,
-      light: (plant.light || 0) + 1
-    }
-  }
-
-  export const changePlantState = (plant, property) => {
-    return {
-      ...plant,
-      [property]: (plant[property] || 0) + 1
-    }
-  }
+export const changePlantState = (plant, property) => {
+  return {
+    ...plant,
+    [property]: (plant[property] || 0) + 1
+  };
+};
 //FUNCTION TO STORE STATE
-  const storeState = () => {
-    let currentState = {};
-    return (stateChangeFunction = state => state) => {
-      const newState = stateChangeFunction(currentState);
-      currentState = {...newState};
-      return newState;
-    }
-  }
+export const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = { ...newState };
+    return newState;
+  };
+};
 
-  export const stateControl = storeState();
+export const stateControl = storeState();
 
-  // const changeState = (state, prop, value) => ({
-  //   ...state,
-  //   [prop] : (state[prop] || 0) + value
-  // })
-
-  // This is a function factory. 
-// We can easily create more specific functions that 
-// alter a plant's soil, water, and light to varying degrees.
-  // CURRIED FUNCTION--vv
-  const changeState = (prop) => {
-    return (value) => {
-      return (state) => ({
-        ...state,
-        [prop] : (state[prop] || 0) + value
-      })
-    }
-  }
-
-  // We create four functions using our function factory. 
-// We could easily create many more.
+// CURRIED FUNCTION--vv
+export const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop]: (state[prop] || 0) + value
+    });
+  };
+};
+ 
 export const feed = changeState("soil")(1);
 export const blueFood = changeState("soil")(5);
 
 export const hydrate = changeState("water")(1);
 export const superWater = changeState("water")(5);
-
-
-
