aa472c18981a4976b1a9612c23e9f14c1502acad
ChrisKLoveless <chriskloveless@gmail.com>
2023-01-30 20:13:01 -0800

    WIP: running with index.html

--- src/js/plant.js
+++ src/js/plant.js
@@ -4,19 +4,19 @@ let plant = {
     light: 0
   }
 
-  const hydrate = (plant) => {
-    return {
-      ...plant,
-      water: (plant.water || 0) + 1
-    }
-  }
+  // const hydrate = (plant) => {
+  //   return {
+  //     ...plant,
+  //     water: (plant.water || 0) + 1
+  //   }
+  // }
 
-  const feed = (plant) => {
-    return {
-      ...plant,
-      soil: (plant.soil || 0) + 1
-    }
-  }
+  // const feed = (plant) => {
+  //   return {
+  //     ...plant,
+  //     soil: (plant.soil || 0) + 1
+  //   }
+  // }
 
   const giveLight = (plant) => {
     return {
@@ -31,18 +31,65 @@ let plant = {
       [property]: (plant[property] || 0) + 1
     }
   }
+//FUNCTION TO STORE STATE
+  const storeState = () => {
+    let currentState = {};
+    return (stateChangeFunction = state => state) => {
+      const newState = stateChangeFunction(currentState);
+      currentState = {...newState};
+      return newState;
+    }
+  }
 
-  const changeState = (state, prop, value) => ({
-    ...state,
-    [prop] : (state[prop] || 0) + value
-  })
+  const stateControl = storeState();
 
-  // CURRIED FUNCTION
-  // const changeState = (prop) => {
+  // const changeState = (state, prop, value) => ({
+  //   ...state,
+  //   [prop] : (state[prop] || 0) + value
+  // })
+
+  // This is a function factory. 
+// We can easily create more specific functions that 
+// alter a plant's soil, water, and light to varying degrees.
+  // CURRIED FUNCTION--vv
+  const changeState = (prop) => {
     return (value) => {
       return (state) => ({
         ...state,
         [prop] : (state[prop] || 0) + value
       })
     }
-  // }
+  }
+
+  // We create four functions using our function factory. 
+// We could easily create many more.
+const feed = changeState("soil")(1);
+const blueFood = changeState("soil")(5);
+
+const hydrate = changeState("water")(1);
+const superWater = changeState("water")(5);
+
+window.onload = function() {
+
+  // This function has side effects because we are manipulating the DOM.
+  // Manipulating the DOM will always be a side effect. 
+  // Note that we only use one of our functions to alter soil. 
+  // You can easily add more.
+  document.getElementById('feed').onclick = function() {
+    const newState = stateControl(blueFood);
+    document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
+  };
+
+  // This function doesn't actually do anything useful in this application 
+  // — it just demonstrates how we can "look" at the current state 
+  // (which the DOM is holding anyway). 
+  // However, students often do need the ability to see the current state 
+  // without changing it so it's included here for reference.
+  document.getElementById('show-state').onclick = function() {
+    // We just need to call stateControl() without arguments 
+    // to see our current state.
+    const currentState = stateControl();
+    document.getElementById('soil-value').innerText = `Soil: ${currentState.soil}`;
+  };
+};
+
